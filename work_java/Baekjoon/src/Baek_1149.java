e46bf7df0420bad40c90d2a0d5052475b9458ffd
Seonhanbit <ok18880@hanmail.net>
2019-10-23 19:51:16 +0900

    1023

--- work_java/Baekjoon/src/Baek_1149.java
+++ work_java/Baekjoon/src/Baek_1149.java
@@ -2,42 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Baek_1149 {
-	static int N, result;
-	static int[][] arr;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N][3];
-		int sum = 0;
-		int su = Integer.MAX_VALUE;
-		int index = 0;
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 3; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if(su>arr[i][j]) {
-					su=arr[i][j];
-					index = i;
-				}
-			}
-		}
-		sum+=su;
-		su = Integer.MAX_VALUE;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < 3; j++) {
-				if(j==index)
-					continue;
-				if(su>arr[i][j])
-					su=arr[i][j];
-			}
-		}
-		sum+=su;
-
-		bw.write(sum + "\n");
-		bw.flush();
-		bw.close();
-	}
-
+	 static class Color{
+	        
+	        int r;
+	        int g;
+	        int b;
+	        
+	        public Color() {}
+	        public Color(int r, int g, int b) {
+	            this.r = r;
+	            this.g = g;
+	            this.b = b;
+	        }
+	        @Override
+	        public String toString() {
+	            return "Color [r=" + r + ", g=" + g + ", b=" + b + "]";
+	        }
+	    }
+	    
+	    public static void main(String[] args) throws NumberFormatException, IOException {
+	        
+	        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	        
+	        int n = Integer.parseInt(bf.readLine());
+	        Color[] c = new Color[n];
+	        int[][] sum = new int[n+1][3];
+	        
+	        for(int i=0; i<n; i++) {
+	            
+	            StringTokenizer st = new StringTokenizer(bf.readLine());
+	            int rcost = Integer.parseInt(st.nextToken());
+	            int gcost = Integer.parseInt(st.nextToken());
+	            int bcost = Integer.parseInt(st.nextToken());
+	            c[i] = new Color(rcost, gcost, bcost);
+	        }
+	        
+	        for(int i=1; i<=n; i++) {
+	            
+	            sum[i][0] = Math.min(sum[i-1][1], sum[i-1][2]) + c[i-1].r;
+	            sum[i][1] = Math.min(sum[i-1][0], sum[i-1][2]) + c[i-1].g;
+	            sum[i][2] = Math.min(sum[i-1][0], sum[i-1][1]) + c[i-1].b;
+	        }
+	        
+	        int min = Integer.MAX_VALUE;
+	        for(int i=0; i<3; i++) {
+	            
+	            if(min > sum[n][i])
+	                min = sum[n][i];
+	        }
+	        
+	        bw.write(min +"\n");
+	        bw.flush();
+	        bw.close();
+	    }
 }
---
+++ work_java/Baekjoon/src/hw_ct_1023.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.util.*;
+
+public class hw_ct_1023 {
+	static int N, res;
+	static int[][] arr;
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		res = (1+(N*N))*N/2;
+		/*
+		 * 마방진 순서 1. 첫 순서는 첫행 가운데에 넣기 
+		 * 2. i가 N의 배수이면 바로 아래의 행으로 이동하여 다음 수 넣기 
+		 * 3. 배수가 아니면, 왼쪽 위로 이동하여 다음 수 넣기 
+		 * 3-1. 행이 0을 벗어나면 마지막 행으로 이동 
+		 * 3-2. 열이 0을 벗어나면 마지막 열로 이동
+		 */
+
+		obb(0, N / 2);
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j)
+				System.out.print(arr[i][j] + " ");
+			System.out.println();
+		}
+		System.out.println(check());
+		bw.write(res + "\n");
+		bw.flush();
+		bw.close();
+	}
+
+	static void obb(int x, int y) {
+		arr[x][y] = 1;
+		for (int i = 2; i <= N * N; i++) {
+			if (i % N == 1) {
+				x++;
+				if (x == N)
+					x = 0;
+				arr[x][y] = i;
+			} else {
+				x--;
+				y--;
+				if (x == -1)
+					x = N - 1;
+				if (y == -1)
+					y = N - 1;
+				arr[x][y] = i;
+			}
+		}
+	}
+
+	static void even() {
+
+	}
+
+	static boolean check() {
+		boolean ck = true;
+		int idx = 0;
+		int sum=0;
+		for (int i = 0; i < N; i++) {
+			sum = 0;
+			for (int j = 0; j < N; j++) {
+				sum += arr[i][j];
+			}
+			if(sum!=res)
+				ck=false;
+		}
+		for (int i = 0; i < N; i++) {
+			sum = 0;
+			for (int j = 0; j < N; j++) {
+				sum += arr[j][i];
+			}
+			if(sum!=res)
+				ck=false;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			sum = 0;
+			for (int j = 0; j < N; j++) {
+				sum += arr[j][i];
+			}
+			if(sum!=res)
+				ck=false;
+		}
+		sum=0;
+		for(int i=0; i<N; i++) {
+			sum+=arr[i][i];
+		}
+		if(sum!=res)
+			ck=false;
+		
+		sum = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(i+j==N-1)
+					sum+=arr[i][j];
+			}
+		}
+		if(sum!=res)
+			ck=false;
+		
+		if (ck)
+			return true;
+		else
+			return false;
+	}
+}
