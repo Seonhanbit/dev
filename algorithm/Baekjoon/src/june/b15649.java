5a8ff17483df2352fbf966838f87f43a88a396ba
Seonhanbit <52940537+Seonhanbit@users.noreply.github.com>
2020-06-23 18:46:09 +0900

    algorithm [+] 문자열 풀이

---
+++ algorithm/Baekjoon/src/june/b15649.java
@@ -0,0 +1,11 @@
+package june;
+
+import java.io.*;
+import java.util.*;
+
+public class b15649 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+}
